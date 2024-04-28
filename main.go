443f6a386481445f27c221952e355da5dacffd79
Dion Fananie <dionfananie@gmail.com>
2024-04-28 19:36:49 +0700

    add main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type user struct {
+    Id        int     `json:"id"`
+    Username  string  `json:"username"`
+}
+
+var users = []user{
+    {Id: 546, Username: "John"},
+    {Id: 894, Username: "Mary"},
+    {Id: 326, Username: "Jane"},
+}
+
+func getUsers(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, users)
+}
+func getHome(c *gin.Context){
+	c.String(http.StatusOK, "hello world")
+}
+func main() {
+    router := gin.Default()
+
+    // new `GET /users` route associated with our `getUsers` function
+    router.GET("/users", getUsers);
+	
+	router.GET("/", getHome);
+
+    router.Run("localhost:8080")
+}
