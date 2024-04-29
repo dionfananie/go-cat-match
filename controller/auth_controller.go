c32ac6bda91151a7546f31349ff69ca01d3c7dea
Anshor J <lordlexa10@gmail.com>
2024-04-29 13:38:12 +0700

    Create login and register handler

---
+++ controller/auth_controller.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"net/http"
+	"web/go-cat-match/database"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type LoginRequest struct {
+	Email string `json:"email" binding:"required,email"`
+}
+
+type RegisterRequest struct {
+	Name     string `json:"name" binding:"required,min=5,max=50"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=5,max=15"`
+}
+
+func Login(c *gin.Context) {
+	var req LoginRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//TODO : Complete this function
+
+	c.JSON(http.StatusOK, gin.H{"data": req})
+}
+
+func Register(c *gin.Context) {
+	var req RegisterRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	database.DB.Exec("INSERT INTO users (name, email, password) VALUES ($1, $2, $3)", req.Name, req.Email, hashedPassword)
+
+	//TODO : Complete this function
+
+	c.JSON(http.StatusOK, gin.H{"data": req})
+}
