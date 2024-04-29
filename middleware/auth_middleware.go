cb902dc86c1a50633f773022132a1d20083c4643
Anshor J <lordlexa10@gmail.com>
2024-04-29 13:38:52 +0700

    Create auth_middleware.go

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Do something here
+
+		c.Next()
+	}
+}
