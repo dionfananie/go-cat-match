aba45648976afb4d8337ba31a078ba94f6d83b5a
Dion Fananie <dionfananie@gmail.com>
2024-04-29 21:23:49 +0700

    feat(auth): add login modul (2)

--- controller/auth_controller.go
+++ controller/auth_controller.go
@@ -32,19 +32,18 @@ func Login(c *gin.Context) {
 	var Name string
 	var Email string
 	var Pass string
+	var UserId uint64
 	println("user checking: ", req.Email)
 
 	// query := "SELECT name, password as pass, email from users WHERE email = ?"
-	err := database.DB.QueryRow("SELECT name, password, email from users WHERE email = $1", req.Email).Scan(&Name, &Pass, &Email)
+	err := database.DB.QueryRow("SELECT id, name, password, email from users WHERE email = $1", req.Email).Scan(&UserId, &Name, &Pass, &Email)
 
 	if err != nil {
-		println("fatal, no rows found")
+		c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
 		return
 
 	}
-
 	errs := password.Verify(Pass, req.Password)
-
 	if errs != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "request doesnt pass validation"})
 		return
@@ -52,9 +51,17 @@ func Login(c *gin.Context) {
 	}
 	println("UserId", Name)
 	println("UserId", Email)
+	println("UserId", UserId)
 
-	c.JSON(http.StatusOK, gin.H{"message": "User logged successfully", "data": gin.H{"email": Email,
-		"name": Name}})
+	accessToken := jwt.Generate(&jwt.TokenPayload{
+		UserId: UserId,
+	})
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged successfully", "data": gin.H{
+		"email":       Email,
+		"name":        Name,
+		"accessToken": accessToken,
+	}})
 }
 
 func Register(c *gin.Context) {
