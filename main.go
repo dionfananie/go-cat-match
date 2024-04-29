17b2fa887ec6b10ec94057099679255a58819277
Anshor J <lordlexa10@gmail.com>
2024-04-29 14:12:34 +0700

    Setup auth middleware

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	router "web/go-cat-match/app"
 	"web/go-cat-match/database"
+	"web/go-cat-match/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	database.Init()
-
 	r := gin.Default()
-	// Setup Auth Middleware
+
+	r.Use(middleware.AuthMiddleware())
 
 	router.SetupRouter(r)
 
