293ce2b77c46a3cd462b66b943d9a1e83b31721f
Anshor J <lordlexa10@gmail.com>
2024-04-29 15:05:29 +0700

    Fix : wrong register api method

--- app/router.go
+++ app/router.go
@@ -9,7 +9,7 @@ import (
 
 func SetupRouter(r *gin.Engine) {
 	r.GET("/v1/login", controller.Login)
-	r.GET("/v1/register", controller.Register)
+	r.POST("/v1/register", controller.Register)
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World!")
