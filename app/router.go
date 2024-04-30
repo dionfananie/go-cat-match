e4c8d4a24f7e61d9239a5f47a761cd08a418e9ec
Anshor J <lordlexa10@gmail.com>
2024-04-30 07:41:18 +0700

    Add example code

--- app/router.go
+++ app/router.go
@@ -11,6 +11,9 @@ func SetupRouter(r *gin.Engine) {
 	r.POST("/v1/user/login", controller.Login)
 	r.POST("/v1/user/register", controller.Register)
 
+	// Route with auth middleware example (only user logged in can access this route)
+	// r.GET("/v1/cat", middleware.AuthMiddleware, controller.SomeMethodHere)
+
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World!")
 	})
